cfa1ea7309932317884b8b286f83e5794bb39c61
Arkadii Ivanov <arkann1985@gmail.com>
2023-08-09 22:08:28 +0100

    Updated Kotlin to 1.9.0

--- runtime/src/darwinMain/kotlin/com/arkivanov/parcelize/darwin/Coding.kt
+++ runtime/src/darwinMain/kotlin/com/arkivanov/parcelize/darwin/Coding.kt
@@ -5,6 +5,7 @@ import platform.Foundation.NSCoder
 import platform.Foundation.NSLock
 import platform.Foundation.decodeObjectOfClass
 import platform.Foundation.encodeObject
+import platform.Foundation.firstObject
 
 fun NSCoder.encodeParcelableOrNull(value: Parcelable?, key: String) {
     val coding = value?.coding()
@@ -14,7 +15,7 @@ fun NSCoder.encodeParcelableOrNull(value: Parcelable?, key: String) {
 @Throws(IllegalStateException::class)
 @Suppress("UNCHECKED_CAST")
 fun <T : Parcelable> NSCoder.decodeParcelableOrNull(key: String): T? =
-    (decodeObjectOfClass(aClass = NSLock, forKey = key) as NSArray?)?.objectAtIndex(0) as T?
+    (decodeObjectOfClass(aClass = NSLock, forKey = key) as NSArray?)?.firstObject as T?
 
 fun NSCoder.encodeParcelable(value: Parcelable, key: String) {
     encodeParcelableOrNull(value, key)
