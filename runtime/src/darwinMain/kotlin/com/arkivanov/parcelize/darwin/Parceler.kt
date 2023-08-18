65589893261f8c6372a39386ba1d3af46f593e64
Arkadii Ivanov <arkann1985@gmail.com>
2023-08-18 17:38:32 +0100

    Optional methods in Parceler interface

--- runtime/src/darwinMain/kotlin/com/arkivanov/parcelize/darwin/Parceler.kt
+++ runtime/src/darwinMain/kotlin/com/arkivanov/parcelize/darwin/Parceler.kt
@@ -10,10 +10,13 @@ interface Parceler<T> {
     /**
      * Creates a new instance of [T] and reads all its data from the provided [coder].
      */
-    fun create(coder: NSCoder): T
+    fun create(coder: NSCoder): T =
+        throw NotImplementedError("Parceler#create is not implemented: $this")
 
     /**
      * Writes the [T] instance state to the provided [coder].
      */
-    fun T.write(coder: NSCoder)
+    fun T.write(coder: NSCoder) {
+        throw NotImplementedError("Parceler#write is not implemented: $this")
+    }
 }
